4a57fe8838ec6785325403922546372ed302af8a
zmy <zhaomingyao271@gmail.com>
2026-07-12 15:28:52 +0800

    修复侧边栏&今日&按钮日期不同步bug

--- data.js
+++ data.js
@@ -154,7 +154,7 @@ function renderReport(r){
 
 function renderSidebar(){
  var reports=A(),list=document.getElementById("dateList");
- var items='<li><button class="active" data-date="2026-07-06"><span class="date-dot"></span>今日 7/6</button></li>';
+ var items='<li><button class="active" data-date="2026-07-12"><span class="date-dot"></span>今日 7/12</button></li>';
  reports.forEach(function(r){var d=new Date(r.date+"T00:00:00");items+='<li><button data-date="'+r.date+'"><span class="date-dot"></span>'+(d.getMonth()+1)+"/"+d.getDate()+'</button></li>'});
  list.innerHTML=items;
  list.querySelectorAll("button").forEach(function(btn){btn.addEventListener("click",function(){list.querySelectorAll("button").forEach(function(b){b.classList.remove("active")});this.classList.add("active");var r=G(this.dataset.date);if(r)renderReport(r)})})
